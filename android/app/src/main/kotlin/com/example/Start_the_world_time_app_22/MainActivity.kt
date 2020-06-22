9b11f683c006e1b3741d877dfcacddc66d979b4b
Hung <hungldgch29091996@gmail.com>
2020-06-22 15:26:03 +0700

    First commit

---
+++ android/app/src/main/kotlin/com/example/Start_the_world_time_app_22/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.Start_the_world_time_app_22
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
